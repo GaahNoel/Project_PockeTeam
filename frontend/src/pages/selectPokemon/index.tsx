654a85539e4c20f3c94f12ba2d170e9d841748f3
unknown <gsn181200@gmail.com>
2020-10-14 19:08:20 -0300

    Changes select pokemon

--- frontend/src/pages/selectPokemon/index.tsx
+++ frontend/src/pages/selectPokemon/index.tsx
@@ -9,6 +9,7 @@ import Header from '../../components/header';
 import './styles.css';
 import imageInterrogation from './interrogation.png'
 import { SportsCricketSharp } from '@material-ui/icons';
+import { Container } from '@material-ui/core';
 
 // import { Container } from './styles';
 
@@ -23,6 +24,15 @@ const SelectPokemon: React.FC = () => {
 
   const [movesArray, setMovesArray]: any = useState([]);
   const [itemsArray, setItemsArray]: any = useState([]);
+  
+  /*Seleções de moves*/
+  const [move1, setMove1]: any = useState('');
+  const [move2, setMove2]: any = useState('');
+  const [move3, setMove3]: any = useState('');
+  const [move4, setMove4]: any = useState('');
+  
+  
+
 
   const changePokemon = (e: any) => {
     if (e) {
@@ -42,7 +52,7 @@ const SelectPokemon: React.FC = () => {
           moves.forEach((move: any) => {
             setMovesArray((oldMoves: any) => [
               ...oldMoves,
-              { value: move.move.name, label: move.move.name },
+              { value: move.move.url, label: move.move.name },
             ]);
           });
         });
@@ -63,6 +73,39 @@ const SelectPokemon: React.FC = () => {
     }
   };
 
+  const requireMoveDetails = (e: any) => {
+
+
+      if (e) {
+        axios
+          .get(e.value).then((response) => {
+            const{accuracy, damage_class:{name: damage_type}, effect_chance, effect_entries, type:{name: type_name}, pp, power} = response.data
+            const effect_name = (effect_entries[0].short_effect).replace("$effect_chance%", effect_chance)
+
+
+          console.log(accuracy)
+          console.log(damage_type)
+          console.log(effect_chance)
+          console.log(effect_name)
+          console.log(type_name)
+          console.log(pp)
+          console.log(power)
+          })
+      }
+  }
+
+  const widthChange = {
+    container:(provide: any, state: any) => ({
+      ...provide,
+      width:170,
+    }),
+
+    input:(provide: any, state: any) => ({
+       
+      paddingRight:25,
+    })
+  }
+
   return (
     <div className="selectPokemonContainer">
       <Helmet>
@@ -79,19 +122,24 @@ const SelectPokemon: React.FC = () => {
          (<img src={imageInterrogation} />)
          }
            
-          
-
-          <div>
-              <Select options={movesArray} name="move1" isDisabled={pokemonName ? false:true}/>
-              <Select options={movesArray} name="move2" isDisabled={pokemonName ? false:true}/>
-              <Select options={movesArray} name="move3" isDisabled={pokemonName ? false:true}/>
-              <Select options={movesArray} name="move4" isDisabled={pokemonName ? false:true}/>
+          <div className="formMoves">
+              <Select options={movesArray} name="move1" styles={widthChange} onChange={(e: any) => {requireMoveDetails(e); setMove1(e.label)}} isDisabled={pokemonName ? false:true}/>
+              <Select options={movesArray} name="move2" onChange={(e: any) => {requireMoveDetails(e); setMove2(e.label)}} isDisabled={pokemonName ? false:true}/>
+              <Select options={movesArray} name="move3" onChange={(e: any) => {requireMoveDetails(e); setMove3(e.label)}} isDisabled={pokemonName ? false:true}/>
+              <Select options={movesArray} name="move4" onChange={(e: any) => {requireMoveDetails(e); setMove4(e.label)}} isDisabled={pokemonName ? false:true}/>
           </div>
           
           <div>
               <Select options={itemsArray} name="item" isDisabled={pokemonName ? false:true}/>
           </div>
 
+          <button  onClick={(e)=>{
+            e.preventDefault()
+            console.log(move1)
+            console.log(move2)
+            console.log(move3)
+            console.log(move4)
+            }}></button>
         </form>
       </div>
     </div>
